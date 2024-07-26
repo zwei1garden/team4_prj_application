d832202d0dc29b761ec8aa371e3eb3ee80472942
Lee Jeong Won <106802869+zwei1garden@users.noreply.github.com>
2024-07-27 00:09:27 +0900

    Update ApiMockController.java

--- src/main/java/ac/su/inclassspringsecurity/apicontroller/ApiMockController.java
+++ src/main/java/ac/su/inclassspringsecurity/apicontroller/ApiMockController.java
@@ -14,7 +14,7 @@ public class ApiMockController {
     @GetMapping
     public ResponseEntity<String> getApiMock() {
         return new ResponseEntity<>(
-            "Calling API root URL is not allowed. leegarden plus and pipeline and webhooks kt-cloud-webhooks-test-leegarden",
+            "Calling API root URL is not allowed. leegarden plus and pipeline and webhooks KTCloud-webhooks-test-leegarden",
             HttpStatus.FORBIDDEN);
     }
 
